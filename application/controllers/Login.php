ace45f9ece01e624b38899881d6134798b3ffaca
Administrator <admin@example.com>
2023-01-23 14:36:15 +0000

    Update application/controllers/Login.php

--- application/controllers/Login.php
+++ application/controllers/Login.php
@@ -7,7 +7,7 @@ class Login extends CI_Controller {
         parent::__construct();
         $this->load->model('LoginModel');
     }
-
+//////
 	public function index()
 	{   
         $this->load->view('_partial/head');

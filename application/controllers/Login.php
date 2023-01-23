1c8e752305c09edd32a7e9d9294478b277b6945c
Administrator <admin@example.com>
2023-01-23 14:35:33 +0000

    Update application/controllers/Login.php

--- application/controllers/Login.php
+++ application/controllers/Login.php
@@ -2,7 +2,7 @@
 defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Login extends CI_Controller {
-
+//check sync
     function __construct(){
         parent::__construct();
         $this->load->model('LoginModel');
